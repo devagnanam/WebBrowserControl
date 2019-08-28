9b1e557c36e6ad6cc2c3f783f98b60ebe3aa95ca
Deva <dpideva@hotmail.com>
2019-08-28 10:21:46 -0700

    Add project files.

---
+++ TestWebBrowserControl/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace TestWebBrowserControl
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 fs1 = new Form2();
+            fs1.Activate();
+            fs1.Show();
+        }
+    }
+}
---
+++ TestWebBrowserControl/Form2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestWebBrowserControl
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+            this.webBrowser1.Url = new Uri("https://www.microsoft.com");
+        }
+    }
+}
